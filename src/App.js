8ce655d7f9e7fe007fbd1afb04188e186df74d92
shaynjay <shaysavidge1@gmail.com>
2021-07-08 17:17:41 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>this is a placeholder</p>
+      <h1></h1>
       <li>hello people</li>
+      <p>this is a placeholder</p>
     </div>
   );
 }
