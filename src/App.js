0437616cb98fc4cf801e69cca8159a1a06559798
shaynjay <shaysavidge1@gmail.com>
2021-07-08 17:22:28 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1></h1>
+      <h1>A place holder</h1>
       <li>hello people</li>
       <p>this is a placeholder</p>
     </div>
