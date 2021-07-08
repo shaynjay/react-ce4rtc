3c0c0c5b5f87d19a9eb4eb94ce96c025a4513e9a
shaynjay <shaysavidge1@gmail.com>
2021-07-08 17:12:05 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>this is a placeholder</p>
+      <li>hello people</li>
     </div>
   );
 }
